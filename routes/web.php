1a90d7afa1a8e2b3dd13a9232ed7fe013425eac0
Joaquin <rosanojuanma4@gmail.com>
2026-09-01 01:23:27 -0300

    1.9.1 joaco

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +12,11 @@ Route::get('/carrito', function () {
     return view('carrito');
 });
 
-Route::get('/login', function () {
-    return view('FormularioIngreso');
-});
+Route::get('/login', [LoginController::class, 'showLoginForm'])
+    ->name('login.form');
+
+Route::post('/login', [LoginController::class, 'login'])
+    ->name('login');
+
+Route::post('/register', [RegistroController::class, 'register'])
+    ->name('register');
